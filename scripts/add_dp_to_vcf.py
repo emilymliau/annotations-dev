bf87fe63e0b1d28b8dcefb8de58a9fd50d8198a2
Emily Liau <emily.m.liau@gmail.com>
2025-01-17 16:17:18 -0500

    modify VCF header lines

--- scripts/add_dp_to_vcf.py
+++ scripts/add_dp_to_vcf.py
@@ -31,13 +31,13 @@ if 'DP' in mt.row:
 # calculate FORMAT-level DP (sum of AD fields per sample)
 print(f"...calculating FORMAT-level DP")
 mt = mt.annotate_entries(DP=hl.sum(mt.AD))
-header['format']['DP'] = {'Description': 'Approximate read depth (estimated as sum of AD per sample).', 'Number': '1', 'Type': 'Integer'}
+header['format']['DP'] = {'Description': 'Approximate read depth, estimated as sum of AD per sample.', 'Number': '1', 'Type': 'Integer'}
 
 # calculate INFO-level DP (sum of AD fields across samples)
 print(f"...calculating INFO-level DP")
 # mt = mt.annotate_rows(info=mt.info.annotate(DP=hl.agg.sum(mt.DP)))
 mt = mt.annotate_rows(info=mt.info.annotate(DP=hl.agg.sum(hl.sum(mt.AD))))
-header['info']['DP'] = {'Description': 'Approximate read depth (estimated as sum of AD across samples).', 'Number': '1', 'Type': 'Integer'}
+header['info']['DP'] = {'Description': 'Approximate read depth, estimated as sum of AD across samples.', 'Number': '1', 'Type': 'Integer'}
 print(f"...DP calculations completed at: {datetime.datetime.now()}")
 
 hl.export_vcf(mt, output_vcf, metadata=header, tabix=True)
