af74bbbe0ab127616eed8f80953b78f05cfb460b
Emily Liau <emily.m.liau@gmail.com>
2025-01-17 09:44:55 -0500

    modify progress statements

--- scripts/add_dp_to_vcf.py
+++ scripts/add_dp_to_vcf.py
@@ -18,7 +18,7 @@ hl.init(min_block_size=128, spark_conf={"spark.executor.cores": cores,
                     "spark.driver.memory": f"{int(np.floor(mem*0.4))}g"
                     }, tmp_dir="tmp", local_tmpdir="tmp")
 
-print(f"Processing started at: {datetime.datetime.now()}")
+print(f"...Processing started at: {datetime.datetime.now()}")
 header = hl.get_vcf_metadata(input_vcf)
 mt = hl.import_vcf(input_vcf, reference_genome=build)
 
@@ -26,13 +26,13 @@ header['info']['DP'] = {'Description': 'Approximate read depth (estimated as sum
 header['format']['DP'] = {'Description': 'Approximate read depth (estimated as sum of AD).', 'Number': '1', 'Type': 'Integer'}
 
 # calculate FORMAT-level DP (sum AD fields per sample)
-print(f"Calculating FORMAT-level DP...")
+print(f"...Calculating FORMAT-level DP")
 mt = mt.annotate_entries(DP=hl.sum(mt.AD))
 
 # calculate INFO-level DP (sum of FORMAT-level DP)
-print(f"Calculating INFO-level DP...")
-mt = mt.annotate_rows(INFO_DP=hl.agg.sum(mt.DP))
+print(f"...Calculating INFO-level DP")
+mt = mt.annotate_rows(DP=hl.agg.sum(mt.DP))
 
 hl.export_vcf(mt, output_vcf, metadata=header, tabix=True)
-print(f"Processing finished at: {datetime.datetime.now()}")
+print(f"...Processing completed at: {datetime.datetime.now()}")
 print("Output VCF: ", output_vcf)
